e1b6e89b3dc9306d9c1f89bb3d77796ffa05b151
Romana Reyna <romanareyna@ya.ru>
2021-01-22 08:26:21 +0300

    feat: implement check

--- src/index.js
+++ src/index.js
@@ -1,3 +1,31 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+    const config = bracketsConfig.join("").split(",").join("");
+    let stack = [];
+
+    for (let i = 0; i < str.length; i++) {
+        let index = config.indexOf(str[i]);
+
+        if (index === -1) {
+            continue;
+        }
+
+        if (config[index] === config[index + 1]) {
+            if (str[i] === str[i + 1]) {
+                i++;
+            } else if (str[i] === stack[stack.length - 1]) {
+                stack.pop();
+            } else {
+                stack.push(str[i]);
+            }
+        } else {
+            if (index % 2 === 0) {
+                stack.push(config[index + 1]);
+            } else {
+                if (stack.pop() !== config[index]) {
+                    return false;
+                }
+            }
+        }
+    }
+    return stack.length === 0;
+};
